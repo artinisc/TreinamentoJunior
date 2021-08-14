943acbc33c5930b092fd71e216256aa8b037b5f3
artinisc <artinisc@hotmail.com>
2021-08-13 23:35:02 -0300

    Vercao 1

---
+++ Treinamento1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Treinamento1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
